b19634062eda8f1d22352434bd3ffcf20158a49b
756224720@qq.com <756224720@qq.com>
2021-03-29 14:13:53 +0800

    update

--- amway/amway_ui/docker/jenkinsFile.groovy
+++ amway/amway_ui/docker/jenkinsFile.groovy
@@ -6,17 +6,21 @@ pipeline {
         docker_image_name='amway'
         docker_container_name='amway'
     }
+    parameters{
+	string(name:'branch',defaultValue:'master',descroption:'git branch')
+    }
     stages {
         stage('同步源码') {
             steps {
-                git url:'git@github.com:Ellie2020-ellie/Hogwarts_works.git'
+                git url:'git@github.com:Ellie2020-ellie/Hogwarts_works.git', branch: '$branch'
             }
         }
         stage('输入内容') {
             steps {
                 sh '''
                     . ~/.bash_profile
-                    echo '${docker_image_name}  +>>>>test>>>+   $docker_container_name'
+                    echo '$?????????>>>>>>>>>>'
+                    echo '$docker_container_name'
                     echo ${chrome}
                     echo ${pwd}
                 '''
